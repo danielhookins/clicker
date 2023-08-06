47894c05509e4e1ab080e28eeacfbec0de365d5d
Daniel Hookins <daniel@hookins.net>
2023-08-06 14:50:45 +0930

    Use delta time

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,17 @@ const int BOX_WIDTH = 50;
 const int BOX_HEIGHT = 50;
 const int WINDOW_WIDTH = 160;
 const int WINDOW_HEIGHT = 144;
-const int BOX_SPEED = 2;
+const float BOX_SPEED = 80.0f;
 
 struct Box {
-    int x, y;
-    int dx, dy;
+    float x, y;
+    float dx, dy;
 
     Box() : x(WINDOW_WIDTH / 2 - BOX_WIDTH / 2), y(WINDOW_HEIGHT / 2 - BOX_HEIGHT / 2), dx(BOX_SPEED), dy(BOX_SPEED) {}
 
-    void moveAndBounce() {
-        x += dx;
-        y += dy;
+    void moveAndBounce(float deltaTime) {
+        x += dx * deltaTime;
+        y += dy * deltaTime;
 
         if (x <= 0 || x + BOX_WIDTH >= WINDOW_WIDTH) {
             dx = -dx;
@@ -28,8 +28,8 @@ struct Box {
 Box box;
 
 // Function to update game state
-void update() {
-    box.moveAndBounce();
+void update(float deltaTime) {
+    box.moveAndBounce(deltaTime);
 }
 
 // Function to handle input
@@ -39,12 +39,12 @@ void input(SDL_Event& event) {
 
 // Function to draw to the screen
 void draw(SDL_Window* window) {
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // White background
     SDL_RenderClear(renderer);
 
     // Draw the box
-    SDL_Rect rect = { box.x, box.y, BOX_WIDTH, BOX_HEIGHT };
+    SDL_Rect rect = { static_cast<int>(box.x), static_cast<int>(box.y), BOX_WIDTH, BOX_HEIGHT };
     SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255); // Blue box
     SDL_RenderFillRect(renderer, &rect);
 
@@ -67,16 +67,25 @@ int main(int argc, char* argv[]) {
 
     // The window is open: enter program loop (see SDL_PollEvent)
     int running = 1;
-    
+    Uint32 lastTime = SDL_GetTicks();
+
     while (running) {
+        // Update Delta Time
+        Uint32 currentTime = SDL_GetTicks();
+        float deltaTime = (currentTime - lastTime) / 1000.0f;  // Convert to seconds
+        lastTime = currentTime;
+
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
             input(event);
+            
+            // Check for the quit message
             if (event.type == SDL_QUIT) {
                 running = 0;
             }
         }
-        update();
+
+        update(deltaTime);
         draw(window);
     }
     
